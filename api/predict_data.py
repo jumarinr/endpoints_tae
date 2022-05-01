8b44d59e89dc53841c5d593ff3bdb0a947f35879
Juan Diego Marín Rodríguez <jumarinr@unal.edu.co>
2022-05-01 18:43:55 -0500

    modelo de kids y abuelos finalizado

--- api/predict_data.py
+++ api/predict_data.py
@@ -1,31 +1,39 @@
 import os
 from xmlrpc.client import Boolean
 import joblib
-from flask import Request, jsonify, Response
+from flask import Request, Response
 import pandas
 
-
-# cargamos la ruta donde está almacenado el modelo de adultos
-RELATIVE_PATH_ABUELOS = '/models/model_adultos.pkl'
 ABSOLUTE_PATH = os.getcwd()
+
+# cargamos la ruta donde está almacenado el modelo de abuelos
+RELATIVE_PATH_ABUELOS = '/models/granparents_model.pkl'
 MODEL_PATH_ABUELOS = ABSOLUTE_PATH + RELATIVE_PATH_ABUELOS
 
-# cargamos el modelo de adultos
-loaded_model_abuelos = joblib.load(open(MODEL_PATH_ABUELOS, 'rb'))
+# cargamos la ruta donde está almacenado el modelo de niños
+RELATIVE_PATH_KIDS = '/models/kids_model.pkl'
+MODEL_PATH_KIDS = ABSOLUTE_PATH + RELATIVE_PATH_KIDS
+
+# cargamos el modelo de abuelos
+MODELO_ABUELOS = joblib.load(open(MODEL_PATH_ABUELOS, 'rb'))
+
+# cargamos el modelo de niños
+MODELO_KIDS = joblib.load(open(MODEL_PATH_KIDS, 'rb'))
 
 
 def get_predict_data_post(request: Request):
     # extraemos los datos de la petición
+    print("ingresa")
     file_uploaded = request.files["file"]
-    data_base_submitted = pandas.read_csv(file_uploaded)
+    print(file_uploaded)
     is_abuelo = request.form.get('isAbuelo')
     is_abuelo = True if is_abuelo == 'true' else False
 
     # definimos el modelo a utilizar
-    model_to_use = loaded_model_abuelos if is_abuelo == True else loaded_model_abuelos
+    model_to_use = MODELO_ABUELOS if is_abuelo == True else MODELO_KIDS
 
     # definimos la data de prueba
-    test_data = data_base_submitted.drop(['life_satisfaction'], axis=1)
+    test_data = pandas.read_csv(file_uploaded, sep=r",|;")
 
     # definimos la predicción
     prediccion = model_to_use.predict(test_data)
